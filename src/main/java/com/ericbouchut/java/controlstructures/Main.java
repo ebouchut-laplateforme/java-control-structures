afc9fde7d898dbfd116064bd97f580341daa1537
Eric Bouchut <ebouchut@gmail.com>
2025-07-28 21:27:58 +0200

    refactor: Remove the now useless  class
    because each class has its own  entry point

--- src/main/java/com/ericbouchut/java/controlstructures/Main.java
+++
@@ -1,9 +0,0 @@
-package com.ericbouchut.java.controlstructures;
-
-public class Main {
-    public static void main(String[] args) {
-        FizzBuzz.main(args);
-
-        Grade.main(args);
-    }
-}
